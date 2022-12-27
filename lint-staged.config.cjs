0d1d51b2645284ea85fc3bd94c8c990ef86a4c8f
Talgat Tairov <tairovtalgat@gmail.com>
2022-12-28 01:52:28 +0600

    lint fix test

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -12,5 +12,7 @@ module.exports = {
         const filesToLint = await removeIgnoredFiles(files);
         return [`eslint --max-warnings=0 ${filesToLint}`];
     },
-    "*.(css|scss|sass)": (files) => [`stylelint --fix --max-warnings=0 ${files}`]
+    "*.(css|scss|sass)": (files) => {
+        return [`stylelint --fix --max-warnings=0 ${files}`]
+    }
 };
