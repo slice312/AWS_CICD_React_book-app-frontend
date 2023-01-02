0181384faf92608756ac826475a5e44923008836
Talgat Tairov <tairovtalgat@gmail.com>
2023-01-02 22:55:12 +0600

    up

---
+++ src/app/footer/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <p className="text-muted mb-0 py-2">© 2019 Bootstrapious All rights reserved.</p>
+        </footer>
+
+
+    );
+
+
+};
